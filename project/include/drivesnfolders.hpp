22dd9cb65994eeebe248a66f6f6ddc5cd08ded92
tlneondo <tlneondo@gmail.com>
2025-12-28 18:10:14 -0500

    wip

--- project/include/drivesnfolders.hpp
+++ project/include/drivesnfolders.hpp
@@ -1,11 +1,15 @@
 #include <string>
 #include <list>
 #include <cstdint>
+#include <filesystem>
+#include <fstream>
+
+namespace fs = std::filesystem;
 
 
 class syncFolder{
     public:
-        enum class folderType{
+        enum class osUseCase{
             LinuxOnly,
             WindowsOnly,
             WindowsOrProton,
@@ -13,35 +17,82 @@ class syncFolder{
             Ignore
         };
 
-        std::string relPath; //relative to drive, will have ./ or / at the beginning
+        fs::path reltoDrivePath; //relative to drive, will have ./ or / at the beginning
         std::int8_t isInUse; //check open file descriptors etc
         std::int32_t folderContentSize; //folder contents
 
         //should this folder be considered something to run under linux only, windows only, 
         //both, does not matter, or special case ignore, which will add whiteout files to upper directory
-        folderType fldType;
+        osUseCase useType;
+
+        //contstruct
+
+        syncFolder(osUseCase useCase, fs::path relDrivePath){
+            useType = useCase;
+            reltoDrivePath = relDrivePath;
+        }
 
 
 };
 
-class steamFolder : public syncFolder{
+class steamWorkshopFolder{
+    public:
+        fs::path reltoDriveContentPath;
+        fs::path WSACFpath; //operations will prepend a ../ to make ../appworkshop_XXX.acf
+
+        //contstruct
+        steamWorkshopFolder(fs::path relDrive,fs::path ){
+            reltoDrivePath = y;
+            WSACFpath = c;
+        }
+};
+
+class steamGameFolder : public syncFolder{
     public:
         std::string gameTitle;
         std::int32_t appid;
-        std::int32_t status;
-        std::string ACFpath; //operations will prepend a ../ to make ../appmanifestXXXX.acf
+        fs::path ACFpath;
+        steamWorkshopFolder wrkshop;
+        //contstruct
+        steamGameFolder(osUseCase useCase, fs::path relDrivePath, fs::path acfPath) : syncFolder(useCase,relDrivePath){
+            ACFpath = acfPath;
+            //check for workshop path
+            // if $ACFPATH/workshop/appworkshop_$APPID.acf exists
+                //wrkshop.reltoDriveContentPath = ./content/$appid
+                //wrkshop.WSACFPath = appworkshop_$appid.acf
+                //else wrkshop = "";
+
+        }
+ 
+        
+
+
 };
+
+
 
 class Drive{
     public:
         std::int32_t numUUID;
-        std::string upperPath;
-        std::string lowerPath;
-        std::string overlayPath;
+        fs::path upperPath;
+        fs::path lowerPath;
+        fs::path overlayPath;
 
         std::int32_t upperFreeSpace;
         std::int32_t lowerFreeSpace;
 
+        //keep track of steamappspath
+        fs::path steamappsPath;
+
         //list of Folders to Track
-        std::list<syncFolder> trackedFolders;
+        std::list<syncFolder> trackedGenericFolders;
+        std::list<steamGameFolder> trackedSteamGameFolders;
+
+        
+        Drive(){}
+
+
+
+        
+
 };
--- project/main.cpp
+++ project/main.cpp
@@ -1,8 +1,37 @@
 #include "./include/drivesnfolders.hpp"
 
+
+int firstRun(){
+
+    //overlay location /mnt/SSDWin
+    //upper /mnt/NVME0/NTFSOverlays/SSDWinUpper
+    //lower /mnt/winOverlay/SSDWinLower
+
+    //consume /etc/fstab to find all overlay entries
+    //use Drive constructors to build drives with the upper,lower, work, directories
+
+    //add generic folder to search 
+        //check for loose files
+    //search drives for Steamapps Folders
+            //create gameobjects for folders
+                //if workshop folders exist
+                    //create workshop objects atached to gameobjects
+
+
+
+
+}
+
 int main(){
 
+    firstRun();
+
+    
 
 
-    return 0;
+
+
+
+
+return 0;
 }
